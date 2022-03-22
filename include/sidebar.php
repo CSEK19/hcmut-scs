cae2c9e192f3048b0553dc6851daeb1f5c23dde7
CSEK19 <70205271+CSEK19@users.noreply.github.com>
2022-03-22 11:24:21 +0700

    Initial commit

---
+++ include/sidebar.php
@@ -0,0 +1,65 @@
+<div class="sidebar app-aside" id="sidebar">
+	<div class="sidebar-container perfect-scrollbar">
+
+		<nav>
+			<!-- start: MAIN NAVIGATION MENU -->
+			<div class="navbar-title">
+				<span>Main Navigation</span>
+			</div>
+			<ul class="main-navigation-menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-home"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Dashboard </span>
+							</div>
+						</div>
+					</a>
+				</li>
+
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Room Managemnent </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="room-list.php">
+								<span class="title"> Room List </span>
+							</a>
+						</li>
+						<!-- <li>
+							<a href="patient-add.php">
+								<span class="title"> PAT001 </span>
+							</a>
+						</li>
+						<li>
+							<a href="patient-list.php">
+								<span class="title"> PAT002 </span>
+							</a>
+						</li>
+
+						<li>
+							<a href="patient-list.php">
+								<span class="title"> PAT003 </span>
+							</a>
+						</li> -->
+
+
+					</ul>
+				</li>
+			</ul>
+			<!-- end: CORE FEATURES -->
+
+		</nav>
+	</div>
+</div>
